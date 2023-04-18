7324217ad8de2cff4412f84a62a1691fddd83443
Jesse Harlan <gotsanity@gmail.com>
2023-04-18 12:53:49 -0700

    added auth example data for employees

--- AuthExample/Data/ApplicationDbContext.cs
+++ AuthExample/Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AuthExample.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuthExample.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<Employee> Employees { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ AuthExample/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthExample.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
